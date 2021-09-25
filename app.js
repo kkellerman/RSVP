f0c292c54a227d144815e6e8f921297e4814f157
Keith K <keithrk1@gmail.com>
2021-09-25 10:59:00 -0400

    empty string after input

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const input = form.querySelector('input');
 form.addEventListener('submit', (e) =>  {
     e.preventDefault();
    const text = input.value;
+   input.value = '';  //create empty string after input
    const ul = document.getElementById('invitedList');
    const li = document.createElement('li');
    li.textContent = text;
